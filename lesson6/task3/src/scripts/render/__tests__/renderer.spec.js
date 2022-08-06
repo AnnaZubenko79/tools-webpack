2bda7f8d879e6e6c2014bb7808e29a9a92d59e17
AnnaZubenko79 <annazubenko79@gmail.com>
2022-08-06 19:59:51 +0300

    solve task3

---
+++ lesson6/task3/src/scripts/render/__tests__/renderer.spec.js
@@ -0,0 +1,7 @@
+import { createCheckbox } from '../renderer.js';
+
+it('should create checkbox', () => {
+  expect(createCheckbox({ done: false, id: '1' })).toEqual(
+    '<input type="checkbox" data-id="1" class="list-item__checkbox">'
+  );
+});
