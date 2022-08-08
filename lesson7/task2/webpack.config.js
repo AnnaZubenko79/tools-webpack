86010962eddc8891d7912f021678f19c19b4397a
AnnaZubenko79 <annazubenko79@gmail.com>
2022-08-08 15:22:37 +0300

    solve task2 in lesson7

--- lesson7/task2/webpack.config.js
+++ lesson7/task2/webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,16 +11,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s?css$/i,
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.s?css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|jpg|gif)$/i,
+        test: /.(jpg|png)$/,
         use: [
           {
             loader: 'url-loader',
             options: {
               limit: 8192,
+              name: '[name].[ext]',
               outputPath: 'images',
             },
           },
@@ -27,6 +34,8 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
